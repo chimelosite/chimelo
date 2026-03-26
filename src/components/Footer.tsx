a0726183886b68158c60a0c1ff656aa035686840
Victor Bertram <BERTRAMVICTOR61@GMAIL.COM>
2026-03-26 15:12:20 -0300

    feat: ajuste remoção de endereço santiago

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -76,10 +76,6 @@ const Footer: React.FC = () => {
                     
                     <p className="mb-1 font-medium text-white">📍 Caxias do Sul:</p>
                     <p className="text-white">Rua Coronel Flores 810 - Sala 100 - Moinho da Estação<br />Bairro São Pelegrino - Caxias do Sul - RS</p>
-                
-                    <p className="mb-1 font-medium text-white">📍 Santiago:</p>
-                    <p className="text-white">Rua Marechal Deodoro, 806 - Centro<br />Santiago/RS - 97700-0004</p>
-           
                   </div>
                 </div>
               </div>
